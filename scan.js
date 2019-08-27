4eac3bc42faef194327f88d0f84d576227583f28
Nat <nat.wrw@gmail.com>
2019-08-27 09:58:39 +0700

    init.

---
+++ scan.js
@@ -0,0 +1,14 @@
+var noble = require('noble')
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+    console.log('Found device with local name: ' + peripheral.advertisement.localName);
+    console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+    console.log();
+});
